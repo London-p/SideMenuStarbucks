a7cc0e22994fffda5f947c792b3d74353574f3a6
London Petway <78987936+London-p@users.noreply.github.com>
2021-04-13 11:44:38 -0700

    Add files via upload

--- SideMenu/SideMenu/CartView.swift
+++ SideMenu/SideMenu/CartView.swift
@@ -12,18 +12,28 @@ struct CartView: View {
     var body: some View {
         VStack {
             if cart1 == true {
+                
+                Button(action: {cart1.toggle()
+                }, label: {Image(systemName: "xmark.circle.fill")
+                    .resizable()
+                    .foregroundColor(.red)
+                    .frame(width: 30, height: 30, alignment: .center)
+                })
+                
                 CoffeContainerView(coffe: "frapp", image: "7")
                 
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                    Text("Check Out")
-                        .frame(width: 312, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                        .background(Color.gray)
-                        .foregroundColor(.white)
-                        .cornerRadius(8.0)
-                        .padding(.top, 10)
-                })
+                
             }
-        }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Check Out")
+                    .frame(width: 312, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(8.0)
+                    .padding(.top, 10)
+
+        })
     }
 }
 
+}
